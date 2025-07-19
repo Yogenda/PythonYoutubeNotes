c7e9e70df50b6b7484aa409f0b3ad9d3107dfb26
Yogendra <yogendrabire61@gmail.com>
2025-07-19 14:29:08 +0530

    session 18

---
+++ class18.py
@@ -0,0 +1,42 @@
+'''
+myList = ['hemant','abhishek','tape','aditya','yogendra']
+print(myList)
+print(myList[3])
+print(myList[-4])
+l1=list((1,2,3,4,5))
+print(l1)
+l2=['Coding',14,5.87, True, 3+7j,'student']
+print(l2)
+l1=[15,4,6,12,9,6,10]
+print(l1)
+l1[0]=30
+print(l1)
+l1[-1]='Coding'
+print(l1)
+print(len(l1))
+l1.append(60)
+print(l1)
+print(len(l1))
+'''
+'''
+l1=[1,2,3,4,5,6,7,8,9,10]
+print(l1[6])
+print(l1[-4])
+l1[6]=15
+print(l1)'''
+# l1=[0,1,2,3,4,5,6,7,8,9,10]
+# print(len(l1))
+# print(l1[0:11])
+# print(l1[:])
+# print(l1[3:])
+# print(l1[3:8])
+# print(l1[0:10])
+# print(l1[0:11:2])
+l1=[1,2,3,4,5,6,7,8,9,10]
+print(l1[::-1])
+print(l1[-1:-11:-1])
+print(l1[-1:-11:-2])
+l1[3]=40
+print(l1)
+l1[0:3]=[10,20,30]
+print(l1)
